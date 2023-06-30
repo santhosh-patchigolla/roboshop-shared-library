db206021e92ffd1882890c5eb239585d90ff56a5
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-06-30 18:53:25 +0530

    sdc

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,7 @@
 // Declaring a functin name is info (In groovy)
 def info(message) {
     echo "hello I'm a function and my name is info"
-    echo "I'm printing the value of message is $message"
+    echo "I'm printing the value of message is ${message}"
 }
 
 // calling the function
