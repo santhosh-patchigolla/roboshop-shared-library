89fded43e06df1715aed2a7364f2682ebc54906a
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-03 16:39:05 +0530

    sdc

--- vars/angular.groovy
+++ vars/angular.groovy
@@ -20,7 +20,7 @@ def call (COMPONENT) {
                 }    
             }
 
-            stage('Code Compile') {
+            stage('Code ComQualitypile') {
                 steps {
                         //sh "npm install"
                 }
