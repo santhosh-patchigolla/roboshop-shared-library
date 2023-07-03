21cc1af1f0045ced6c43d630309284d6ee306425
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-03 16:06:47 +0530

    sdc

---
+++ vars/python.groovy
@@ -0,0 +1,53 @@
+def lintChecks(){
+    sh '''
+        echo Installing JSLint for ${COMPONENT}
+        pip install pylint
+        echo Installing lint checks done for the ${COMPONENT}
+
+    '''
+}
+
+
+def call (COMPONENT) {
+    pipeline {
+        agent { label 'work-station' }
+        stages {
+
+            stage ('Lint Check') {                     
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }    
+            }
+
+            stage('Code Compile') {
+                steps {
+                        sh "npm install"
+                }
+            }                                              
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//                     sh "echo installing JSlint"
+//                     sh "npm i jslint"
+//                     sh "node_modules/jslint/bin/jslint.js server.js || true"   // this double pipe symbol includes that if the first command is failure make it as a true statement  (that pipe symbol makes forece fully passes the stage)
+
+
+// def info(message) {
+//     echo "hello I'm a function and my name is info"
+//     echo "I'm printing the value of message is ${message}"
+// }
+
+// // calling the function
+// info("hi")
