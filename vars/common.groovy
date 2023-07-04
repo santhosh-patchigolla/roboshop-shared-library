115426c831fa3a7cea963202a5bc674fc3566397
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-04 17:54:23 +0530

    sdc

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,4 @@
 def sonarChecks(){
-    sh 
        sh "echo Sonar Checks starts"
        sh "sonar-scanner -Dsonar.host.url=http://172.31.89.102:9000  -Dsonar.projectKey=${COMPONENT} ${ARGS} -Dsonar.login=${SONARCRED_USR} -Dsonar.password=${SONARCRED_PSW}"
        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh"
