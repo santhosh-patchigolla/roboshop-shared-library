eb44a779ad252547616afc8ce3d044eb9c2288a9
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-05 08:00:39 +0530

    sdc

--- vars/java.groovy
+++ vars/java.groovy
@@ -11,7 +11,7 @@ def lintChecks(){
 def call (COMPONENT) {
     pipeline {
         agent { label 'work-station' }
-        enviornment {
+        environment {  
             SONARCRED = credentials('SONARCRED')
             SONATURL = "172.31.89.102"
         }
