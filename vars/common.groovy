628bb958cae54a96a77f720726080c8c36bacbce
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-07 17:22:45 +0530

    df

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,37 @@
+def lintChecks(){
+    stage('Lint Checks') {
+        if(env.APP_TYPE == "nodejs") {
+        sh  ''' 
+           echo Installing JSLint for ${COMPONENT}
+           npm i jslint  
+           node_modules/jslint/bin/jslint.js server.js || true
+           echo lint checks completed for ${COMPONENT}        
+           ''' 
+        }
+        else if(env.APP_TYPE == "python") {
+        sh '''
+            # pip3 install pylint
+            # pip *.py
+            echo lint checks started for ${COMPONENT} using pytlint
+            echo lint checks completed for ${COMPONENT}  
+        ''' 
+        }
+        else if(env.APP_TYPE == "java") {
+        sh '''
+            echo lint checks start for ${COMPONENT} using mvn
+            # mvn checkstyle:check
+            echo lint checks completed for ${COMPONENT}  
+        ''' 
+        }
+        else  {
+        sh '''
+            echo lint checks started for ${COMPONENT} using angular js
+            echo lint checks completed for ${COMPONENT}  
+         ''' 
+        }
+    }
+}
+
 def sonarChecks(){
        sh "echo Sonar Checks starts"
        // sh "sonar-scanner -Dsonar.host.url=http://172.31.89.102:9000  -Dsonar.projectKey=${COMPONENT} ${ARGS} -Dsonar.login=admin -Dsonar.password=password"
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,112 +1,110 @@
-def lintChecks() {
-    sh '''
-        echo Installing JSLint for ${COMPONENT}
-        npm i jslint
-        node_modules/jslint/bin/jslint.js server.js || true
-        echo Installing lint checks done for the ${COMPONENT}
-    '''
+def call () {
+    node {
+        common.lintChcecks()
+    }
 }
 
 
-def call (COMPONENT) {
-    pipeline {
-        agent { label 'work-station' }
-        environment {
-            SONARCRED = credentials('SONARCRED')
-            NEXUS = credentials('NEXUS')             // given the credential ovet the Jenkins pipeline
-            SONARURL = "172.31.89.102"
-            NEXUSURL = "172.31.93.234"
+   // uncommited for doing due to scripted pipeline
+// def call (COMPONENT) {
+//     pipeline {
+//         agent { label 'work-station' }
+//         environment {
+//             SONARCRED = credentials('SONARCRED')
+//             NEXUS = credentials('NEXUS')             // given the credential ovet the Jenkins pipeline
+//             SONARURL = "172.31.89.102"
+//             NEXUSURL = "172.31.93.234"
             
-        }
-        stages {
+//         }
+//         stages {
 
-            stage ('Lint Check') {                     
-                steps {
-                    script {
-                        lintChecks()
-                    }
-                }    
-            }
+//             stage ('Lint Check') {                     
+//                 steps {
+//                     script {
+//                         lintChecks()
+//                     }
+//                 }    
+//             }
 
-            stage('Code Compile') {
-                steps {
-                        sh "npm install"
-                }
-            }
+//             stage('Code Compile') {
+//                 steps {
+//                         sh "npm install"
+//                 }
+//             }
 
-            stage('Sonar Checks') {
-                steps {
-                    script {  
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarChecks()
-                    }                                // calling the sonar checks function from line11
-                }
-            }
-            stage('TestCases') {
-                parallel {
-                    stage ('Unit Testing') {
-                        steps {
-                            sh "echo unit testing started"
-                            sh "echo unit testing done"
-                        }
-                    }
-                    stage ('Integration Testing') {
-                        steps {
-                            sh "echo Integration testing started"
-                            sh "echo Integration testing done"
-                        }
-                    }
-                    stage ('Funtional Testing') {
-                        steps {
-                            sh "echo Functional testing started"
-                            sh "echo Functional testing done"
-                        }
-                    }
-                }
-            }
+//             stage('Sonar Checks') {
+//                 steps {
+//                     script {  
+//                         env.ARGS="-Dsonar.sources=."
+//                         common.sonarChecks()
+//                     }                                // calling the sonar checks function from line11
+//                 }
+//             }
+//             stage('TestCases') {
+//                 parallel {
+//                     stage ('Unit Testing') {
+//                         steps {
+//                             sh "echo unit testing started"
+//                             sh "echo unit testing done"
+//                         }
+//                     }
+//                     stage ('Integration Testing') {
+//                         steps {
+//                             sh "echo Integration testing started"
+//                             sh "echo Integration testing done"
+//                         }
+//                     }
+//                     stage ('Funtional Testing') {
+//                         steps {
+//                             sh "echo Functional testing started"
+//                             sh "echo Functional testing done"
+//                         }
+//                     }
+//                 }
+//             }
 
-            stage('Verify the artifact version') {
-                when { expression { env.TAG_NAME != null } }        
-                steps {
-                    script {
-                        env.UPLOAD_STATUS=sh (returnStdout: true, script: 'curl -L -s  http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME} || true' )    // true is used for the If the first statement is failed will treat as true
-                        print UPLOAD_STATUS                     
-                    }
-                }
-            }
+//             stage('Verify the artifact version') {
+//                 when { expression { env.TAG_NAME != null } }        
+//                 steps {
+//                     script {
+//                         env.UPLOAD_STATUS=sh (returnStdout: true, script: 'curl -L -s  http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME} || true' )    // true is used for the If the first statement is failed will treat as true
+//                         print UPLOAD_STATUS                     
+//                     }
+//                 }
+//             }
 
-            stage('Prepare Artifact') {
-                when { 
-                    expression { env.TAG_NAME != null }             // when a tag is deducted then it will trigger
-                    expression { env.UPLOAD_STATUS == "" }                                    
-                }      
-                steps {
-                    sh '''
-                        echo prepare Artifacts ${COMPONENT}
-                        npm install
-                        zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+//             stage('Prepare Artifact') {
+//                 when { 
+//                     expression { env.TAG_NAME != null }             // when a tag is deducted then it will trigger
+//                     expression { env.UPLOAD_STATUS == "" }                                    
+//                 }      
+//                 steps {
+//                     sh '''
+//                         echo prepare Artifacts ${COMPONENT}
+//                         npm install
+//                         zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
                        
-                       '''  
-                }
-            }
+//                        '''  
+//                 }
+//             }
 
-            stage('Upload Artifacts') {
-                when { 
-                    expression { env.TAG_NAME != null }
-                    expression { env.UPLOAD_STATUS == "" }                   
-                }        
-                steps {
-                    sh '''
-                        echo uploading artifacts to Nexus
-                        curl -f -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.93.234:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-                        echo Uploading ${COMPONENT} Artifacts To Nexus is done
+//             stage('Upload Artifacts') {
+//                 when { 
+//                     expression { env.TAG_NAME != null }
+//                     expression { env.UPLOAD_STATUS == "" }                   
+//                 }        
+//                 steps {
+//                     sh '''
+//                         echo uploading artifacts to Nexus
+//                         curl -f -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.93.234:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+//                         echo Uploading ${COMPONENT} Artifacts To Nexus is done
 
-                        '''  
-                }    
-            }                                                                                          
-        }
-    }  
-}
+//                         '''  
+//                 }    
+//             }                                                                                          
+//         }
+//     }  
+// }
 
 
 
