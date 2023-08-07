b47590015a4ae2aab479f4fb7be431c86fc8150a
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-07 22:02:54 +0530

    sdc

--- vars/backendinfra.groovy
+++ vars/backendinfra.groovy
@@ -1,8 +1,9 @@
 def call() {
     properties([
             parameters([
-                choice(choices: 'dev\nprod', description: "Select environment", name: "ENV"),
-                choice(choices: 'apply\ndestroy', description: "Chose the action", name: "ACTION")
+                choice(choices: 'dev\nprod', description: "Select your environment", name: "ENV"),
+                choice(choices: 'apply\ndestroy', description: "Chose an action", name: "ACTION"),
+                string(choices: 'APP_VERSION', description: "Enter your backend version", name: "APP_VERSION")
             ]),
         ])
     node {
@@ -10,23 +11,30 @@ def call() {
             git branch: 'main', url: "https://github.com/santhosh-patchigolla/${REPONAME}.git"
             
             stage('terraform init') {
-                sh "cd ${TFDIR}"
-                sh "ls -ltr"
-                sh "terrafile -f env-${ENV}/Terrafile"
-                sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"            
+                sh ''' 
+                    cd ${TFDIR}
+                    ls -ltr
+                    terrafile -f env-${ENV}/Terrafile
+                    terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars  
+                '''       
             }
 
             stage('terraform plan') {
-                sh "cd ${TFDIR}"
-                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+                sh '''
+                    cd ${TFDIR}
+                    terraform plan -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
+                ''' 
             }
 
             stage('Terraform Action') {
-                sh "cd ${TFDIR}"
-                sh "terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
+                sh '''
+                    cd ${TFDIR}
+                    terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
+                '''
                 }
             }
         }
     }
 
 
+            
