2057e1eb8c13acd2015650871d014d1defe56519
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-08 12:01:31 +0530

    sdc

--- vars/backendinfra.groovy
+++ vars/backendinfra.groovy
@@ -7,27 +7,30 @@ def call() {
         ])
     node {
         ansiColor('xterm') {
-            git branch: 'main', url: "https://github.com/b54-clouddevops/${REPONAME}.git
+            git branch: 'main', url: "https://github.com/b54-clouddevops/${REPONAME}.git"
             
             stage('terraform init') {
-                sh "cd ${TFDIR}"
-                sh "ls -ltr"
-                sh "terrafile -f env-${ENV}/Terrafile"
-                sh  "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
+                sh ''' 
+                    cd ${TFDIR}
+                    ls -ltr
+                    terrafile -f env-${ENV}/Terrafile
+                    terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars  
+                '''       
             }
 
             stage('terraform plan') {
-                sh "cd ${TFDIR}"
-                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"   
+                sh '''
+                    cd ${TFDIR}
+                    terraform plan -var-file=env-${ENV}/${ENV}.tfvars 
+                ''' 
             }
 
             stage('Terraform Action') {
-                sh "cd ${TFDIR}"
-                sh  "terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
+                sh '''
+                    cd ${TFDIR}
+                    terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars 
+                '''
                 }
             }
         }
     }
-
-
-            
